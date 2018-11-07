313e36055be2aef3235c0c16ab76d9a62437e12c
root <root@DESKTOP-TQKIH2R.localdomain>
2018-11-07 16:33:17 +0300

    Done

--- week12/ex2.c
+++ week12/ex2.c
@@ -7,19 +7,31 @@
 int main(int argc, char* argv[]){
 	FILE ** files = malloc(sizeof(FILE)*20);
 	int is_append = 0;
+	char * open_mod = "w";
 	if (strcmp(argv[1], "-a") == 0){
 		is_append = 1;
+		open_mod = "a";
 	}
-	for (int i = 1; i < argc; i++){
-//		printf("%s ", argv[i]);
-		files[i+is_append] = fopen(argv[i], "w");
+	files[0] = stdout;
+	for (int i = 1+is_append; i < argc; i++){
+		files[i-is_append] = fopen(argv[i], open_mod);
 	}
-	char *buffer = malloc(100);
-
+	argc = argc - is_append;
+	char *buffer = malloc(BUFFER_SIZE);
+	int max = BUFFER_SIZE;
 	while (fread(buffer, 1, BUFFER_SIZE, stdin)){
-		for (int i = 1; i < argc; i++){
-			fwrite(buffer, 1, BUFFER_SIZE, files[i+is_append]);
+		for (int i = 0; i < BUFFER_SIZE; i++){
+			if (buffer[i] == '\0') {
+				max = i;
+				break;
+			}
 		}
-		printf("%s", buffer);
+		for (int i = 0; i < argc; i++){
+			fwrite(buffer, 1, max, files[i]);
+		}
+		//printf("%s", buffer);
+	}
+	for (int i = 0; i < argc; i++){
+		fclose(files[i]);
 	}
 }
